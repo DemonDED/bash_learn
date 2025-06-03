6df2a60540933fe81d624e5d9ed90b82c2ac33e7
Shella Celinda <shellace@se-b4.nsk.21-school.ru>
2025-06-03 16:37:39 +0700

    Введение, пользовательские и системные переменные

--- bash_01.sh
+++ bash_01.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Переменные среды
 
+echo "Home for the current user: $HOME"
+echo "I have \$1 in my pocket"
+
+# Пользовательские переменные
+
+grade=5
+person="Adam"
+
+echo "$person is a god boy, he is in grade $grade"
